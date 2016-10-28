b2a52cc3b56cc20a8270901eb49fa40f3b0156be
Sebastian Campos <seabasscampos@gmail.com>
2016-10-28 14:30:31 -0400

    Add files via upload
    Fixed a problem occurring when uploading to Github using Windows.

--- removefirstline.sh
+++ removefirstline.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -n +2 "ip_list.txt" > "ip_list.tmp" && mv "ip_list.tmp" "ip_list.txt" #delete first IP, to check for the next IP
+tail -n +2 "ip_list.txt" > "ip_list.tmp" && mv "ip_list.tmp" "ip_list.txt"
