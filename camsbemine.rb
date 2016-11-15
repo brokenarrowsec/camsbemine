08e5351a029fed1c61bfe7ca260bc526d80415c5
Sebastian Campos <seabasscampos@gmail.com>
2016-11-15 18:11:23 -0500

    Add files via upload

--- camsbemine.rb
+++ camsbemine.rb
@@ -1,5 +1,5 @@
 #!/bin/ruby
 
-system("head -n 1 ip_list.txt > victim.txt")
+system("sed -n '1,1p' ip_list.txt > victim.txt")
 system("wget -i victim.txt -O script.html")
 system("ruby sitechecker.rb")
