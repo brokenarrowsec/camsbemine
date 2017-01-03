404dc3333163c4ac5b86ebfe03b46f6ea0f54815
Sebastian Campos <seabasscampos@gmail.com>
2017-01-02 20:10:14 -0500

    Update filefinder.rb

--- filefinder.rb
+++ filefinder.rb
@@ -3,13 +3,12 @@
 finder = system("grep successfully checkthisfile.txt") #Check for success of Hydra attack
 
 if finder == true
-  print "Great! You have successfully gained access to a security camera! Please open 'checkthisfile.txt', for the IP address of the camera, as well as the credentials.
+  print "Great! You have successfully gained access to a security camera! Please open 'openme.txt', for the IP address of the camera, as well as the credentials.
 "
   system("chmod +x removefirstline.sh")
   system("./removefirstline.sh")
 else
   system("chmod +x removefirstline.sh")
   system("./removefirstline.sh")
-  system("chmod +x attack.sh")
-  system("./attack.sh")
+  system("ruby camsbemine.rb")
 end
